c071c49fae12236a5d9dc6f4e09247243d93a0aa
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-03-07 13:53:54 +0100

    Remove duplicates in LatField and LngField validators

--- api/models/fields.py
+++ api/models/fields.py
@@ -2,21 +2,28 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 
+def add_validator_if_not_exist(validators, validator):
+    for existing in validators:
+        if type(existing) == type(validator):
+            return validators
+    validators.append(validator)
+    return validators
+
+
 class LngField(models.FloatField):
     def __init__(self, *args, **kwargs):
-        kwargs["validators"] = [
-            *kwargs.get("validators", []),
-            MinValueValidator(-180),
-            MaxValueValidator(180),
-        ]
+        # Otherwise, the validator is added multiple times
+        validators = kwargs.get("validators", [])
+        add_validator_if_not_exist(validators, MinValueValidator(-180))
+        add_validator_if_not_exist(validators, MaxValueValidator(180))
+        kwargs["validators"] = validators
         super().__init__(*args, **kwargs)
 
 
 class LatField(models.FloatField):
     def __init__(self, *args, **kwargs):
-        kwargs["validators"] = [
-            *kwargs.get("validators", []),
-            MinValueValidator(0),
-            MaxValueValidator(90),
-        ]
+        validators = kwargs.get("validators", [])
+        add_validator_if_not_exist(validators, MinValueValidator(0))
+        add_validator_if_not_exist(validators, MaxValueValidator(90))
+        kwargs["validators"] = validators
         super().__init__(*args, **kwargs)
